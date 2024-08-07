4b2684590f21fb6ca63f9bdf5704736e516c603a
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-08-08 04:32:00 +0530

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -232,7 +232,7 @@ async def Ouo(bot, cmd: Message):
             bl = b['bypassed_link']
             await asyncio.sleep(1)
             await x1.delete()
-          """ bl_markup = InlineKeyboardMarkup(
+            """ bl_markup = InlineKeyboardMarkup(
                           [
                             [
                               InlineKeyboardButton(text="🔗 DESTINATION LINK", url=bl)
